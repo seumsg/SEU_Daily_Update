26fe3600b2f393d368599e7277a382021322d01e
Waikkii <50266226+Waikkii@users.noreply.github.com>
2021-03-20 20:01:06 +0800

    Update Tui.py

--- Tui.py
+++ Tui.py
@@ -9,3 +9,8 @@ def server_post(Subject, Message, Sckey):
 def kutui_post(Subject, Message, Kutui_key):
     url = 'https://push.xuthus.cc/send/' + Kutui_key + '?c=' + Subject + '\n\n' + Message
     r = requests.post(url)
+
+def xuyuantu_bot_markdown(Subject, Message, botkey):
+    url = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=' + botkey
+    markdown = {'msgtype': Subject, 'markdown': Message}
+    r = requests.post(url, json=markdown)
