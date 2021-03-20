8587648c0c7f9d8e68667ee9fc82a2ce471db913
Waikkii <50266226+Waikkii@users.noreply.github.com>
2021-03-20 20:50:19 +0800

    Update Tui.py

--- Tui.py
+++ Tui.py
@@ -12,5 +12,5 @@ def kutui_post(Subject, Message, Kutui_key):
 
 def xuyuantu_bot_markdown(Subject, Message, Botkey):
     url = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=' + Botkey
-    markdown = {'msgtype': markdown, 'markdown': Message}
+    markdown = {'msgtype': 'markdown', 'markdown': Message}
     r = requests.post(url, json=markdown)
