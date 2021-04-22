1d67c467f65b279c209774b8672ce2c6bead4d22
Waikkii <50266226+Waikkii@users.noreply.github.com>
2021-04-22 14:49:41 +0800

    Update auto_temperature.py

--- auto_temperature.py
+++ auto_temperature.py
@@ -175,7 +175,7 @@ if __name__ == '__main__':
         }
         subject = '体温上报失败！'
         #xuyuantu_bot_markdown(subject, content_fail, weixin_botkey)
-        server_post(subject, msg, serverchan_sckey)
+        bark_post(subject, msg, serverchan_sckey)
         print("推送成功，体温上报失败")
         #kutui_post(subject, msg, kutui_key)
 
@@ -189,6 +189,6 @@ if __name__ == '__main__':
         }
         subject = '体温上报成功！'
         #xuyuantu_bot_markdown(subject, content_success, weixin_botkey)
-        server_post(subject, msg, serverchan_sckey)
+        bark_post(subject, msg, serverchan_sckey)
         print("推送成功，体温上报成功")
         #kutui_post(subject, msg, kutui_key)
